1f905f63c3f6402dfa0e3817e9ffcf3d7594dfc1
99283 <99283791@qq.com>
2019-10-23 22:45:17 +0800

    Add 吴章杰

---
+++ src/b2512/wuzhangjie/Main.java
@@ -0,0 +1,39 @@
+package b2512.wuzhangjie;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * 10
+        * 12
+        * 14
+        * 16
+        * 18
+        * */
+        int j=10;
+        int a= 5;
+        j += (a-1) * 2;
+        System.out.print(j);
+
+//        int str[] = {1,3,5,4,2,6};
+//        int j=0;
+//        for (int i=0;i<25;i++,j++){
+//            if (j>=str.length-1){
+//                j=0;
+//            }
+
+//            for (int k = 0;k<str.length;k++) System.out.print(str[k]);
+//            System.out.println();
+
+//            if (str[j]<str[j+1]){
+//                int temp = str[j];
+//                str[j] = str[j+1];
+//                str[j+1] = temp;
+//            }
+//
+//        }
+//
+//        for (int k=0;k<str.length;k++){
+//            System.out.println(str[k]);
+//        }
+    }
+}
