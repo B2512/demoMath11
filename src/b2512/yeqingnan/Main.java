d7a1ba5973fc21c1a9ec8929eae1536efa12c64b
Martw <91098897@qq.com>
2019-10-23 22:45:19 +0800

    Add yeqingnan

---
+++ src/b2512/yeqingnan/Main.java
@@ -0,0 +1,16 @@
+package b2512.yeqingnan;
+
+public class Main {
+    public static void main(String[] args) {
+        /*int age = 0;
+        for (int i = 1; i < 5; i++) {
+            age += 2;
+        }
+        System.out.println(age);*/
+
+        int age = 10;
+        int people = 5;
+        age += (people - 1) * 2;
+        System.out.println(age);
+    }
+}
